1a829359f16bf05d44db39c1fbddf82f2e76b8ba
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-02-14 20:04:28 +0500

    update

--- src/components/layout/HowWeWork.js
+++ src/components/layout/HowWeWork.js
@@ -17,10 +17,12 @@ const HowWeWork = () => {
             <h2>Biz qanday ishlaymiz?</h2>
             <p>Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu. Nam facilisis lacinia ex, eget <br/> sollicitudin massa pellentesque in. Vivamus mattis eros at sem pulvinar</p>
             <div className="container">
-            <div className="how-we-work">   
-            <img src={step} alt=""className="left-img"/>
+            <div className="how-we-work">
+            <div className="media">   
+            <div className="image"><img src={step} alt=""className="left-img"/></div>
             <div className="col-md-2">
                 <p><span style={{fontWeight: 'bold'}}>Talablarni aniqlab chiqamiz</span><br/>Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu.<br/> Nam facilisis</p>
+            </div>
             </div>
             <div className="media flipped"> 
             <div className="image"><img src={problem} alt="" className="right-img"/></div>
@@ -28,9 +30,11 @@ const HowWeWork = () => {
                 <p><span style={{fontWeight: 'bold'}}>Bir necha yechimlarni taklif qilamiz</span><br/>Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu.<br/> Nam facilisis</p>
             </div>
             </div>
-            <img src={project} alt="" className="left-img"/>
+            <div className="media">
+            <div className="image"><img src={project} alt="" className="left-img"/></div>
             <div className="col-md-2">
                 <p><span style={{fontWeight: 'bold'}}>Loyiha uchun vaqt belgilaymiz</span><br/>Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu.<br/> Nam facilisis</p>
+            </div>
             </div>
             <div className="media flipped"> 
             <div className="image"><img src={success} alt="" className="right-img"/></div>
@@ -38,9 +42,11 @@ const HowWeWork = () => {
                 <p><span style={{fontWeight: 'bold'}}>A’lo sifat bilan bajarib topshiramiz</span><br/>Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu.<br/> Nam facilisis</p>
             </div>
             </div>
-            <img src={support} alt="" className="left-img"/>
+            <div className="media">
+            <div className="image"><img src={support} alt="" className="left-img"/></div>
             <div className="col-md-2">
                 <p><span style={{fontWeight: 'bold'}}>Qo’llab-quvvatlab boramiz</span><br/>Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu.<br/> Nam facilisis</p>
+            </div>
             </div>
             </div>
             </div>
